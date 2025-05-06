e7b6acf735ab6647758b39dacba909ea1f93202f
Kareem Adel <kareem.adel20047@gmail.com>
2025-05-06 05:10:02 +0300

    changed the structure to Oauth 2.0

--- Auth/middleware/verifyToken.js
+++ Auth/middleware/verifyToken.js
@@ -1,15 +1,16 @@
 const { verifyToken } = require('../utils/jwt');
 
-const verifyRefreshToken = (req, res, next) => {
-  const token = req.cookies.refresh_token;
+const verifyAccessToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) return res.status(401).json({ detail: 'Missing refresh token' });
+  if (!token) return res.status(401).json({ detail: 'Missing access token' });
 
   const payload = verifyToken(token);
-  if (!payload) return res.status(403).json({ detail: 'Invalid or expired token' });
+  if (!payload) return res.status(403).json({ detail: 'Invalid or expired access token' });
 
   req.user = payload;
   next();
 };
 
-module.exports = verifyRefreshToken;
+module.exports = verifyAccessToken;
