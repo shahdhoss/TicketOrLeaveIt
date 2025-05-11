d69ab84bef3d22fabdcc0b281455b8b2988fdd5f
rana7azem <ranahazem816@gmail.com>
2025-05-11 08:39:09 +0300

    edit on package.json

--- ticket-service/ticket/controllers/Controller.js
+++ ticket-service/ticket/controllers/Controller.js
@@ -5,11 +5,6 @@ const config = require('../config');
 
 class Controller {
   static sendResponse(response, payload) {
-    /**
-    * The default response-code is 200. We want to allow to change that. in That case,
-    * payload will be an object consisting of a code and a payload. If not customized
-    * send 200 and the payload as received in this method.
-    */
     response.status(payload.code || 200);
     const responsePayload = payload.payload !== undefined ? payload.payload : payload;
     if (responsePayload instanceof Object) {
@@ -29,12 +24,6 @@ class Controller {
   }
 
   /**
-  * Files have been uploaded to the directory defined by config.js as upload directory
-  * Files have a temporary name, that was saved as 'filename' of the file object that is
-  * referenced in request.files array.
-  * This method finds the file and changes it to the file name that was originally called
-  * when it was uploaded. To prevent files from being overwritten, a timestamp is added between
-  * the filename and its extension
   * @param request
   * @param fieldName
   * @returns {string}
